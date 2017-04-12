7367572529ebab2b3b929b55ab9aaef76427251a
Matthew Moyer <matthewrmoyer@gmail.com>
2017-04-12 15:47:35 -0600

    add cors

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var posts = require('./routes/posts');
 require('dotenv').config()
+var cors = require('cors')
+
 
 var app = express();
 var PORT = process.env.PORT || 3000
@@ -25,6 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/posts', posts);
+app.use(cors())
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
