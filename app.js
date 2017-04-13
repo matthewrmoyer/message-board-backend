f449a3b00b4e8f3879a47082aa9b98c6b4ac8894
Matthew Moyer <matthewrmoyer@gmail.com>
2017-04-13 09:39:53 -0600

    add posttable  post route

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var PORT = process.env.PORT || 3000
 
 
 
+app.use(cors())
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -25,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors())
+
 app.use('/', index);
 app.use('/posts', posts);
 
